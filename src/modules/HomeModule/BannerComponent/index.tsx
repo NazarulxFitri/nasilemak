07ebea0288d18e0b5392f6acbc94e5f7d827f42e
Mohd Nazarul Fitri <mohd-nazarul-fitri_mohd-azman@astro.com.my>
2023-03-03 16:12:50 +0800

    Use styled mui to custom styling

--- src/modules/HomeModule/BannerComponent/index.tsx
+++ src/modules/HomeModule/BannerComponent/index.tsx
@@ -1,30 +1,31 @@
-import { Box, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 import { Lobster } from "next/font/google";
+import { styled } from "@mui/material/styles";
 
 const lobster = Lobster({ subsets: ["latin"], weight: "400" });
+
+const MainBanner = styled("h1")(({ theme }) => ({
+  color: "#FFF",
+  fontSize: "68px",
+  margin: "16px auto 0",
+  textAlign: "center",
+  textShadow: "1px 12px #468429",
+  width: "fit-content",
+  [theme.breakpoints.up("md")]: {
+    fontSize: "112px",
+  },
+}));
 
 const BannerComponent = () => {
   return (
     <Box pb={4}>
-      <Typography
-        variant="h1"
-        className={lobster.className}
-        sx={{
-          fontFamily: "unset",
-          color: "#FFF",
-          fontSize: { xs: "68px", md: "112px" },
-          margin: "16px auto 0",
-          textAlign: "center",
-          textShadow: "1px 12px #468429",
-          width: "fit-content",
-        }}
-      >
+      <MainBanner className={lobster.className}>
         <span style={{ color: "#2F5C1A" }}>N</span>
         asi <span style={{ color: "#2F5C1A" }}>L</span>emak
         <br />
         <span style={{ color: "#2F5C1A" }}>C</span>ik{" "}
         <span style={{ color: "#2F5C1A" }}>S</span>iti
-      </Typography>
+      </MainBanner>
     </Box>
   );
 };
